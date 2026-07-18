fe8903da71462770a9bdf0baec97c8b938141f62
deMel0729 <malandemel00@gmail.com>
2026-07-18 13:14:36 +0530

    Allowed React to Access Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'corsheaders',
 
 ]
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
